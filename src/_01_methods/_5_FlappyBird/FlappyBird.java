c19655d6dc4b8864c4053bc03b25b19d114a7d3b
lvl0_module2 <Eddiejhunter1000000@gmail.com>
2025-06-13 12:54:53 -0700

    htht

--- src/_01_methods/_5_FlappyBird/FlappyBird.java
+++ src/_01_methods/_5_FlappyBird/FlappyBird.java
@@ -1,6 +1,7 @@
 package _01_methods._5_FlappyBird;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class FlappyBird extends PApplet {
     static final int WIDTH = 800;
@@ -18,8 +19,15 @@ public class FlappyBird extends PApplet {
 
     @Override
     public void draw() {
-        
+    	PImage bird;
+    	
+    	bird = loadImage("images/bird.png");
+    	bird.resize(40, 40);
+        image(bird, 50, 300);
+       
     }
+       
+                 
 
     static public void main(String[] args) {
         PApplet.main(FlappyBird.class.getName());
