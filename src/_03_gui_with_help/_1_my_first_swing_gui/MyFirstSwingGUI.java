4f2f334d4eb1eed2c9e2f38dded1d211081026c3
league <maddoxseul@sbsd.net>
2025-08-23 12:55:17 -0700

    b

--- src/_03_gui_with_help/_1_my_first_swing_gui/MyFirstSwingGUI.java
+++ src/_03_gui_with_help/_1_my_first_swing_gui/MyFirstSwingGUI.java
@@ -16,34 +16,34 @@ public class MyFirstSwingGUI {
 	public void run() {
 
 		// 1. Create and initialize an object of the JFrame class
-
+			JFrame window = new JFrame();
 		// 2. Set your JFrame object to be visible
-
+	
 		// 3. Run your program. Do you see your window? It's probably very
 		// small.
 
 		// 4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-
+			window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 5. Create and initialize an object of the JPanel class
-
+			JPanel solar = new JPanel();
 		// 6. Create and initialize an object of the JLabel class
-
+			JLabel text = new JLabel();
 		// 7. Set the text of the JLabel to a lovely greeting.
-
+			text.setText("Hello, thank you for coming");
 		// 8. Add the JPanel object to the JFrame
-
+			window.add(solar);
 		// 9. Add the JLabel object to the JPanel
-
+			solar.add(text);
 		// 10. Pack your JFrame.
-
+			window.pack();
 		// 11. Run your program again. Do you see your message.
 
 		// 12. Use the loadImage method to set the icon of the JLabel object.
-
+			text.setIcon(loadImage());
 		// 13. Re-pack the JFrame object.
-
+			window.pack();
 		// 14. Run the program one more time. Do you see the image?
-
+			window.setVisible(true);
 	}
 
 	public ImageIcon loadImage() {
--- src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
+++ src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
@@ -19,31 +19,37 @@ public class PhotoQuiz {
 	public void run() {
 
 		JFrame quizWindow = new JFrame();
-		quizWindow.setVisible(true);
+		
 		// This will make sure the program exits when you close the window
 		quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address” )
-
+		String link = "https://static.wikia.nocookie.net/zelda_gamepedia_en/images/1/15/TotK_Link_Artwork.png/revision/latest/scale-to-width-down/1200?cb=20240221141140";
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component comp;
 		// 3. use the "createImage()" method below to initialize your Component
-
+		comp = createImage(link);
 		// 4. add the image to the quiz window
-
+		quizWindow.add(comp);
 		// 5. call the pack() method on the quiz window
+		quizWindow.pack();
+		quizWindow.setVisible(true);
 
 		// 6. ask a question that relates to the image
-
+		String zelda = JOptionPane.showInputDialog("Do you like the legend of zelda");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if(zelda.equalsIgnoreCase("yes")) {
+			JOptionPane.showMessageDialog(null, "CORRECT");
+			}else {
+				JOptionPane.showMessageDialog(null, "INCORRECT");
+			}
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+		
 		// 10. find another image and create it (might take more than one line
 		// of code)
 
@@ -54,7 +60,7 @@ public class PhotoQuiz {
 		// 13. ask another question
 
 		// 14+ check answer, say if correct or incorrect, etc.
-
+	
 	}
 
 	private Component createImage(String imageUrl) {
