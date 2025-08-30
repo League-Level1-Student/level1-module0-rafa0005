7607ba9e514dc8ef5dc73c1c160a9afc39747b7a
league <bobby>
2025-08-30 12:53:45 -0700

    stuff

--- src/_03_gui_with_help/_3_drum_kit/DrumKit.java
+++ src/_03_gui_with_help/_3_drum_kit/DrumKit.java
@@ -6,6 +6,7 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
@@ -17,10 +18,12 @@ public class DrumKit implements MouseListener {
 
     public void run() {
         //  Make a JFrame variable and initialize it using "new JFrame()"
-
+    	JFrame window = new JFrame();
         //  Make the frame visible and
         // set its default close operation to JFrame.EXIT_ON_CLOSE
-
+    	window.setVisible(true);
+    	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	
         //  Set the title of the frame
 
         //  Make a JPanel variable and initialize it using "new JPanel().
