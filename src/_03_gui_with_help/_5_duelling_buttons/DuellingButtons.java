0700f1721de261f7dfb82344629d7e15a7c42912
league <league@imac-26.attlocal.net>
2025-10-04 12:57:46 -0700

    b

--- src/_03_gui_with_help/_5_duelling_buttons/DuellingButtons.java
+++ src/_03_gui_with_help/_5_duelling_buttons/DuellingButtons.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class DuellingButtons implements ActionListener {
@@ -27,40 +28,50 @@ public class DuellingButtons implements ActionListener {
 	public void run() {
 
 		// 1. Add the panel to the frame
-
+		frame.add(panel);
 		// 2. Make the frame visible
-
+		frame.setVisible(true);
 		// 3. Set the text of the leftButton to "Click me!"
-
+		leftButton.setText( "click me!");
 		// 4. Set the text of the rightButton to "Click me!"
-
+		rightButton.setText("click me");
 		// 5. Add an action listener to the leftButton
-
+		leftButton.addActionListener(this);
 		// 6. Add an action listener to the rightButton
-
+		rightButton.addActionListener(this);
 		// 7. Add the leftButton to the panel
-
+		panel.add(leftButton);
 		// 8. Add the rightButton to the panel
-
+		panel.add(rightButton);
 		// 9. Pack the frame
-
+		frame.pack();
 		// 10. Set the title of the frame to "Demanding Buttons"
-
+		frame.setTitle("Demanding Buttons");
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		JButton buttonPressed = (JButton) arg0.getSource();
-
+		JOptionPane.showMessageDialog(null, "button pressed");
 		/* If the buttonPressed was the leftButton.... */
 		// Set the text of the rightButton to "No, click Me!"
 		// Set the PREFERRED size of the rightButton to BIG
 		// Set the text of the leftButton to "Click Me!"
 		// Set the PREFERRED size of the leftButton to SMALL
 
-		
-		
-		
+		if(leftButton == buttonPressed) {
+			JOptionPane.showMessageDialog(null, "The left button has been pressed");
+			rightButton.setText("No click me!");
+			rightButton.setPreferredSize(BIG);
+			leftButton.setPreferredSize(SMALL);
+		}
+		if(rightButton == buttonPressed) {
+			JOptionPane.showMessageDialog(null, "The right button has been pressed");
+			leftButton.setText("No click me!"); 
+			leftButton.setPreferredSize(BIG);
+			rightButton.setPreferredSize(SMALL);
+		}
+		panel.repaint();
 		
 		/* If the buttonPressed was the rightButton, do the opposite. */
 
---
+++ src/_04_gui_from_scratch/_1_fortune_cookie/fortunecookie.java
@@ -0,0 +1,16 @@
+package _04_gui_from_scratch._1_fortune_cookie;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class fortunecookie {
+	JFrame frame = new JFrame();
+	JButton button = new JButton();
+public  void showButton() {
+	System.out.println("Button clicked");
+	
+	frame.setVisible(true);
+}
+
+
+}
